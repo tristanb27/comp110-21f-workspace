0715a9c0bd4085f355003e0589723966e887b820
Tristan Blizzard <tristanb@email.unc.edu>
2021-08-29 17:55:40 -0400

    Changed > to < relational operator

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -8,7 +8,7 @@ grthan: bool = lhs2 < rhs2
 greqto: bool = lhs2 >= rhs2 
 eqto: bool = lhs2 == rhs2
 exeq: bool = lhs2 != rhs2
-print(lhs2 + " > " + rhs2 + " is " + str(grthan))
+print(lhs2 + " < " + rhs2 + " is " + str(grthan))
 print(lhs2 + " >= " + rhs2 + " is " + str(greqto))
 print(lhs2 + " == " + rhs2 + " is " + str(eqto))
 print(lhs2 + " != " + rhs2 + " is " + str(exeq))
